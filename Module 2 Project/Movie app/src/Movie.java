d02723c7e5c517d9e42dcb3c29c6ae5c0ba684c8
Daniel <dannysht27@gmail.com>
2021-10-21 10:48:03 +0200

    Fixed my issue

--- Module 2 Project/Movie app/src/Movie.java
+++ Module 2 Project/Movie app/src/Movie.java
@@ -1,6 +1,10 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
+enum Genre {
+    ACTION, COMEDY, DRAMA, FANTASY, HORROR, MYSTERY, ROMANCE, THRILLER
+}
+
 public class Movie {
     private ArrayList<Actor> cast = new ArrayList<Actor>();
     private String title;
