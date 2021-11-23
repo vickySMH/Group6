911eef1d5e4be4587b4daf546a6df5c0bd0f77c4
Kevin Williams <90599747+NooGames@users.noreply.github.com>
2021-11-23 13:50:56 +0100

    Adds Room File

--- Module 3 Project/Hotel application/src/Database.java
+++ Module 3 Project/Hotel application/src/Database.java
@@ -1,16 +1,66 @@
-import People.Staff;
-
-import java.util.ArrayList;
-
 public class Database
 {
+    private static File rooms;
+    private static FileInputStream fis;
+    private static ObjectInputStream ois;
+    private static FileOutputStream fos;
+    private static ObjectOutputStream output;
+
     private static ArrayList<Staff> staff = new ArrayList<>();
-    public static void loadStaff()
-    {
-    }
+    private static ArrayList<Room> roomList = new ArrayList<>();
 
     public static ArrayList<Staff> getStaff()
     {
         return staff;
     }
+
+    public static void loadRooms() throws IOException, ClassNotFoundException
+    {
+        fis = new FileInputStream("rooms.ser");
+        ois = new ObjectInputStream(fis);
+        roomList = (ArrayList<Room>) ois.readObject();
+        ois.close();
+    }
+    private static void openRooms() throws IOException
+    {
+        rooms = new File("rooms.ser");
+        fos = new FileOutputStream(rooms, false);
+        output = new ObjectOutputStream(fos);
+    }
+
+    public static ArrayList<Room> getRoomList() {
+        return roomList;
+    }
+
+    private static void closeRooms() throws IOException
+    {
+        output.close();
+    }
+
+    public static void addRoom(Room room)
+    {
+        roomList.add(room);
+        addRoomToDatabase();
+    }
+
+    public static void saveDatabase()
+    {
+        addRoomToDatabase();
+    }
+
+    private static void addRoomToDatabase(){
+
+        try
+        {
+            openRooms();
+            output.writeObject(roomList);
+            closeRooms();
+        }
+        catch(IOException e)
+        {
+            System.out.println("Error in database, room not found");
+        }
+    {
+    }
+
 }
