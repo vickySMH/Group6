a4ba331064cb7f9d916ee4e8dc8c22f9f236a892
Daniel Shterev <59877556+Dannysht@users.noreply.github.com>
2021-11-23 13:47:50 +0100

    Update Run.java

--- Module 3 Project/Hotel application/src/Run.java
+++ Module 3 Project/Hotel application/src/Run.java
@@ -31,18 +31,18 @@ public class Run
                 if(command[0].equalsIgnoreCase("staff")
                         && staff.getTitle().equalsIgnoreCase("manager"))
                 {
-                    for(Staff personnel : Database.getStaff())
+                    for(Staff personel : Database.getStaff())
                     {
-                        System.out.println(personnel);
+                        System.out.println(personel);
                     }
                 }
                 if(command[0].equalsIgnoreCase("help"))
                 {
-
+                    helpMessage();
                 }
                 else
                 {
-                    System.out.println("Unknown command, please check \'help \' for list of commands");
+                    System.out.println("Unknown command, please check 'help ' for list of commands");
                     cmd = reader.readLine();
                     command = cmd.split(" ", 2);
                     break;
@@ -54,14 +54,45 @@ public class Run
     public void startMessage() throws IOException
     {
         System.out.println("Welcome to Kaizen's hotel app! " + "\n" +
-                "For more information please, type in \'help\'!");
+                "For more information please, type in 'help'!");
         cmd = reader.readLine();
         command = cmd.split(" ", 2);
     }
 
     public void helpMessage() throws IOException
     {
-        System.out.println("help message");
+        if(staff.getTitle().equalsIgnoreCase("manager"))
+        {
+            System.out.println("Staff - displays all of the staff working in the hotel");
+            System.out.println("Add - displays a message asking if you would like to add a room or staff");
+            System.out.println("Remove - displays a message asking if you would like to remove a room or staff");
+            System.out.println("Change - displays a message asking if you would like to change a room or staff");
+            System.out.println("Payroll - checks staff's salaries");
+            System.out.println("Dirty - displays all of the dirty rooms in the hotel");
+            System.out.println("Bookings - displays all booked rooms");
+        }
+
+        if(staff.getTitle().equalsIgnoreCase("cleaner"))
+        {
+            System.out.println("Clean - marks room as no longer dirty");
+            System.out.println("Dirty - displays all of the dirty rooms in the hotel");
+        }
+
+        if(staff.getTitle().equalsIgnoreCase("receptionist"))
+        {
+            System.out.println("Book - books a room for a customer");
+            System.out.println("Change - changes a booking for a customer");
+            System.out.println("Remove - removes a booking for a customer");
+            System.out.println("Bookings - displays all booked rooms");
+        }
+
+        if(staff.getTitle().equalsIgnoreCase("accountant"))
+        {
+            System.out.println("Income - checks income of hotel");
+            System.out.println("Tax - checks taxes that the hotel has to pay");
+            System.out.println("Payroll - checks staff's salaries");
+            System.out.println("Budget - checks budget for hotel needs");
+        }
         cmd = reader.readLine();
         command = cmd.split(" ", 2);
     }
