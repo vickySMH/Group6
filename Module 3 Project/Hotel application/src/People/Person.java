bc05501117a22515c22026cccc74dcea2e8a4c7b
ventsi002 <90601170+ventsi002@users.noreply.github.com>
2021-11-26 11:54:13 +0100

    minor changes

--- Module 3 Project/Hotel application/src/People/Person.java
+++ Module 3 Project/Hotel application/src/People/Person.java
@@ -5,16 +5,21 @@ abstract class Person
 
     protected String fullName;
     protected String phoneNumber;
+    protected int ID
 
     public Person()
     {
         fullName = "";
         phoneNumber = "";
     }
-    public Person(String firstName, String lastName, String phoneNumber)
+    public Person(String firstName, String lastName, String phoneNumber, int ID)
     {
         fullName = firstName + " " + lastName;
         this.phoneNumber = phoneNumber;
+    }
+    
+    public int getID() {
+        return ID;
     }
 
 }
