32732f8b2f53119893f0ca8b3cb9be88f28c7660
vickySMH <90599550+vickySMH@users.noreply.github.com>
2022-03-10 13:29:53 +0100

    Update AdminController.java

--- Second Semester/Daycare/src/main/java/com/example/daycare/AdminController.java
+++ Second Semester/Daycare/src/main/java/com/example/daycare/AdminController.java
@@ -1,5 +1,89 @@
 package com.example.daycare;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
-public class AdminController
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AdminController implements Initializable
 {
+
+    @FXML
+    private Button view;
+
+    @FXML
+    private Button add;
+
+    @FXML
+    private Button update;
+
+    @FXML
+    private Button remove;
+
+    @FXML
+    private Button back;
+
+    @FXML
+    private AnchorPane toView;
+
+    @FXML
+    private AnchorPane toAdd;
+
+    @FXML
+    private AnchorPane toUpdate;
+
+    @FXML
+    private AnchorPane toRemove;
+
+
+    public void initialize(URL url, ResourceBundle resourceBundle){
+
+        toView.setVisible(false);
+        toAdd.setVisible(false);
+        toUpdate.setVisible(false);
+        toRemove.setVisible(false);
+
+        view.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                toView.setVisible(true);
+            }
+        });
+
+        add.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                toAdd.setVisible(true);
+            }
+        });
+
+        update.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                toUpdate.setVisible(true);
+            }
+        });
+
+        remove.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                toRemove.setVisible(true);
+            }
+        });
+
+        back.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+
+            }
+        });
+
+    }
+
 }
