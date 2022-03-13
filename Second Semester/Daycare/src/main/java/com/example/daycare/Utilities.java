8611b0573a596fdbade3094b84ac4d1c18d06f96
Daniel <dannysht27@gmail.com>
2022-03-13 19:09:33 +0100

    Created connection between main and loggedin screen

--- Second Semester/Daycare/src/main/java/com/example/daycare/Utilities.java
+++ Second Semester/Daycare/src/main/java/com/example/daycare/Utilities.java
@@ -1,7 +1,14 @@
 package com.example.daycare;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.sql.*;
 
 public class Utilities
@@ -16,41 +23,81 @@ public class Utilities
     private static String user = System.getenv("user");
     private static String password = System.getenv("password");
     
-//    public static void changeScene(ActionEvent event, String fmxlFile, String username, String password){
-//        Parent root = null;
-//
-//        if(user != null && password != null){
-//            try {
-//                FXMLLoader loader = new FXMLLoader(Utilities.class.getResource(fmxlFile));
-//                root = loader.load();
-//                LoggedInController loggedInController = loader.getController();
-//            }
-//            catch (IOException e){
-//                e.printStackTrace();
-//            }
-//        }
-//        else{
-//            try{
-//                root = FXMLLoader.load(Utilities.class.getResource(fmxlFile));
-//            }
-//            catch (IOException e){
-//                e.printStackTrace();
-//            }
-//        }
-//    }
+    public static void changeScene(ActionEvent event, String fmxlFile, String title ,String username, String password)
+    {
+        Parent root = null;
+
+        if(username != null)
+        {
+            try
+            {
+                FXMLLoader loader = new FXMLLoader(Utilities.class.getResource(fmxlFile));
+                root = loader.load();
+                LoggedInController loggedInController = loader.getController();
+            }
+            catch (IOException e)
+            {
+               e.printStackTrace();
+            }
+        }
+        else
+        {
+            try
+            {
+                root = FXMLLoader.load(Utilities.class.getResource(fmxlFile));
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setTitle(title);
+        stage.setScene(new Scene(root, 2880 ,1800));
+        stage.setMaximized(true);
+        stage.show();
+    }
 
     public static void loginUser(ActionEvent event, String username, String password)
     {
         try
         {
             connection();
-            preparedStatement = connection.prepareStatement("SELECT password, from Daycare.users where username = ?; ");
+            preparedStatement = connection.prepareStatement("SELECT password from Daycare.users where username = ?; ");
             preparedStatement.setString(1, username);
             resultSet = preparedStatement.executeQuery();
+            if(!resultSet.isBeforeFirst())
+            {
+                System.out.println("User not found");
+                Alert alert = new Alert(Alert.AlertType.ERROR, "User not found!");
+                alert.show();
+            }
+            else
+            {
+                while (resultSet.next())
+                {
+                    String retrievePassword = resultSet.getString("password");
+                    if(retrievePassword.equals(password))
+                    {
+                        changeScene(event, "logged-in.fxml", "Welcome!", username, password);
+                    }
+                    else
+                    {
+                        System.out.println("Credentials do not match!");
+                        Alert alert = new Alert(Alert.AlertType.ERROR, "Invalid credentials!");
+                        alert.show();
+                    }
+                }
+            }
+
         }
         catch (SQLException e)
         {
-
+            e.printStackTrace();
+        }
+        finally
+        {
+            closeConnection();
         }
     }
 
@@ -63,6 +110,64 @@ public class Utilities
         catch (SQLException e)
         {
             e.printStackTrace();
+        }
+    }
+    private static void closeConnection()
+    {
+        if(resultSet != null)
+        {
+            try
+            {
+                resultSet.close();
+            }
+            catch (SQLException e)
+            {
+                e.printStackTrace();
+            }
+        }
+        if(preparedStatement != null)
+        {
+            try
+            {
+                preparedStatement.close();
+            }
+            catch (SQLException e)
+            {
+                e.printStackTrace();
+            }
+        }
+        if(preparedStatementInsert != null)
+        {
+            try
+            {
+                preparedStatementInsert.close();
+            }
+            catch (SQLException e)
+            {
+                e.printStackTrace();
+            }
+        }
+        if(preparedStatementUserExists != null)
+        {
+            try
+            {
+                preparedStatementUserExists.close();
+            }
+            catch (SQLException e)
+            {
+                e.printStackTrace();
+            }
+        }
+        if (connection != null)
+        {
+            try
+            {
+                connection.close();
+            }
+            catch (SQLException e)
+            {
+                e.printStackTrace();
+            }
         }
     }
 
