7e11d753098ccce7c0ae5ca50c5570287b50521d
vickySMH <90599550+vickySMH@users.noreply.github.com>
2022-03-18 08:45:10 +0100

    Update Utilities.java

--- Second Semester/Daycare/src/main/java/com/example/daycare/Utilities.java
+++ Second Semester/Daycare/src/main/java/com/example/daycare/Utilities.java
@@ -1,5 +1,7 @@
 package com.example.daycare;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
