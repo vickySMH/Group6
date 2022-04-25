086c0101ed9725005d7ed6d3c0305da472b2f831
ventsi002 <viliev2002@gmail.com>
2022-04-25 11:42:03 +0200

    spring

---
+++ Second Semester/bouncing_balls_Spring/src/main/java/com/example/bouncing_balls/BouncingBallsApplication.java
@@ -0,0 +1,15 @@
+package com.example.bouncing_balls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BouncingBallsApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(BouncingBallsApplication.class, args);
+    }
+
+}
---
+++ Second Semester/bouncing_balls_Spring/src/test/java/com/example/bouncing_balls/BouncingBallsApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.bouncing_balls;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BouncingBallsApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
