a2e607e64c2d2a9197daa51dc71ed10e05945856
ventsi002 <viliev2002@gmail.com>
2022-04-28 13:41:29 +0200

    nordic motorhome

---
+++ Second Semester/NordicMotorhome/src/main/java/com/example/nordicmotorhome/NordicMotorhomeApplication.java
@@ -0,0 +1,15 @@
+package com.example.nordicmotorhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NordicMotorhomeApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(NordicMotorhomeApplication.class, args);
+    }
+
+}
---
+++ Second Semester/NordicMotorhome/src/test/java/com/example/nordicmotorhome/NordicMotorhomeApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.nordicmotorhome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NordicMotorhomeApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
